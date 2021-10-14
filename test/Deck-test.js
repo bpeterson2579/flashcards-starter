67e9373a8e2a3c1774f32b46f1536874179614a5
Brian Peterson <bpeterson1579@gmail.com>
2021-10-13 20:05:37 -0600

    Complete Deck test writing

---
+++ test/Deck-test.js
@@ -0,0 +1,47 @@
+const assert = require('chai').assert;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+    assert.isFunction(Deck);
+  });
+
+  it.skip('should be an instance of Deck', function() {
+    const deck = new Deck();
+
+    assert.instanceOf(deck, Deck);
+  });
+
+  it.skip('should store multiple cards in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const cards = [card1, card2, card3];
+
+    const deck = new Deck(cards);
+
+    assert.equal(deck.cards, cards);
+  });
+
+  it.skip('should count the number of cards in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1]);
+
+    assert.equal(deck.countCards(), 1);
+
+    deck.cards.push(card2);
+
+    assert.equal(deck.countCards(), 2);
+
+    deck.cards.push(card3);
+
+    assert.equal(deck.countCards(), 3);
+  })
+})
